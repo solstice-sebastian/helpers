0896303e6b8344c3ebd96b33444ab506425d070b
solstice-sebastian <solstice.sebastian@gmail.com>
2018-08-07 17:00:08 -0700

    initial commit

---
+++ helpers.js
@@ -0,0 +1,141 @@
+/* eslint valid-typeof: 'off' */
+const moment = require('moment-timezone');
+const glob = require('glob');
+const fs = require('fs');
+const { DATETIME, DATETIME_FILENAME } = require('@solstice.sebastian/constants');
+
+const Helpers = () => {
+  /**
+   * @param price {Number}
+   * @param mod {Number} percentage in float form i.e. 2% => 0.02 || -2% => -0.02
+   */
+  const modByPercent = (price, mod, digits = 10) => +(price * (1 + mod)).toFixed(digits);
+
+  /**
+   * return random number between min/max
+   *
+   * @param {Number} min
+   * @param {Number} max
+   */
+  const rand = (min, max) => Math.random() * (max - min) + min;
+
+  /**
+   * @param start {Number}
+   * @param end {Number}
+   * @return float representation of a percent i.e. 2% => 0.02 || -2% => -0.02
+   */
+  const getPercentDiff = (start, end, digits = 10) => {
+    const _start = +start;
+    const _end = +end;
+
+    let diff;
+    if (_end < _start) {
+      diff = (_start - _end) / _start * -1;
+    } else {
+      diff = (_end - _start) / _start;
+    }
+    return +diff.toFixed(digits);
+  };
+
+  const nicePercent = (percent, digits = 2) => `${(percent * 100).toFixed(digits)}%`;
+
+  const globDelete = (globStr) =>
+    new Promise((res, rej) => {
+      glob(globStr, (err, files) => {
+        if (err) {
+          rej(err);
+        } else {
+          files.forEach(fs.unlinkSync);
+          res();
+        }
+      });
+    });
+
+  const noop = () => {};
+
+  const datetimeForFilename = () => moment().format(DATETIME_FILENAME);
+  const datetime = () => moment().format(DATETIME);
+  const msToDatetime = (ms) => moment(ms).tz('America/Los_Angeles').format(DATETIME);
+
+  const toQueryString = (obj) => {
+    let str = '';
+    Object.keys(obj).forEach((key) => {
+      if (obj[key] !== undefined) {
+        str = `${str}&${key}=${obj[key]}`;
+      }
+    });
+    return str.replace(/&$/, '').replace(/^&/, '');
+  };
+
+  /**
+   * @param {Object} required [key] { type, validator }
+   * @param {Object} params to validate
+   * @return {Error[]} errors
+   */
+  const validateRequired = (required, params, shouldThrow = false) => {
+    const requiredKeys = Object.keys(required);
+    const errors = [];
+
+    Object.keys(required).forEach((key) => {
+      const value = params[key];
+      const requiredItem = required[key];
+
+      if (requiredKeys.includes(key) === true) {
+        if (value === undefined) {
+          // is required but undefined
+          errors.push(`Missing param '${key}'`);
+        } else if (
+          typeof requiredItem.validator === 'function' &&
+          requiredItem.validator(value) === false
+        ) {
+          errors.push(`Failed validator function for param '${key}'`);
+        } else if (requiredItem.type !== undefined && typeof value !== requiredItem.type) {
+          // is required but wrong type
+          errors.push(`Incorrect type for param '${key}'. Received '${typeof value}'`);
+        }
+      }
+    });
+
+    if (errors.length > 0 && shouldThrow === true) {
+      throw new Error(errors.join('\n'));
+    } else {
+      return errors.map((msg) => new Error(msg));
+    }
+  };
+
+  const safeJson = (response) => {
+    if (response.status === 200) {
+      return response.json();
+    }
+    console.log(`response.status:`, response.status);
+    console.log(`response.statusText:`, response.statusText);
+    return response;
+  };
+
+  const castSatoshi = (number) => +number.toFixed(8);
+
+  const toSatoshi = (number) => +(+number * 0.00000001).toFixed(8);
+
+  return {
+    modByPercent,
+    getPercentDiff,
+    noop,
+    datetime,
+    datetimeForFilename,
+    globDelete,
+    msToDatetime,
+    nicePercent,
+    toQueryString,
+    validateRequired,
+    castSatoshi,
+    toSatoshi,
+    safeJson,
+    rand,
+  };
+};
+
+if (typeof module !== 'undefined') {
+  module.exports = Helpers;
+} else if (typeof window !== 'undefined') {
+  window.Helpers = Helpers;
+}
---
+++ tests/helpers.js
@@ -0,0 +1,102 @@
+const Helpers = require('../helpers.js');
+const Constants = require('@solstice.sebastian/constants');
+const test = require('tape');
+
+test(`modByPercent`, (assert) => {
+  const { modByPercent } = Helpers();
+  assert.equal(modByPercent(42, 0.02, 2), 42.84, 'should increase by percent');
+  assert.equal(modByPercent(42, -0.02, 2), 41.16, 'should descrease by percent');
+  const posFloat = 42.123432;
+  assert.equal(modByPercent(posFloat, 0.02, 2), 42.97, 'should increase by percent');
+  assert.equal(modByPercent(posFloat, -0.02, 2), 41.28, 'should descrease by percent');
+  assert.end();
+});
+
+test(`getPercentDiff`, (assert) => {
+  const { getPercentDiff } = Helpers();
+  assert.equal(getPercentDiff(42, 42.84, 2), 0.02, 'should return percent increase as float');
+  assert.equal(getPercentDiff(42, 41.28, 2), -0.02, 'should return percent descrease as float');
+  assert.equal(getPercentDiff(0.00083, 0.00099, 2), 0.19, 'should work on fractions: increase');
+  assert.equal(getPercentDiff(0.00099, 0.00083, 2), -0.16, 'should work on fractions: decrease');
+  assert.end();
+});
+
+test(`nicePercent`, (assert) => {
+  const { nicePercent } = Helpers();
+  const p = 0.12345678;
+  assert.equal(nicePercent(p), '12.35%', 'should default to 2 places and round');
+  assert.equal(nicePercent(p, 0), '12%', 'should take places param');
+  assert.equal(nicePercent(p, 3), '12.346%', 'should take places param');
+  assert.end();
+});
+
+test(`toQueryString`, (assert) => {
+  const { toQueryString } = Helpers();
+  const input = {
+    key1: 'val1',
+    key2: 'val2',
+  };
+  const expected = 'key1=val1&key2=val2';
+  assert.equal(toQueryString(input), expected);
+  assert.equal(
+    toQueryString(Object.assign({}, input, { bogus: undefined })),
+    expected,
+    'should not add empty vals'
+  );
+  assert.end();
+});
+
+test(`validateRequired`, (assert) => {
+  const { validateRequired } = Helpers();
+  const required = {
+    key1: {
+      type: 'string',
+    },
+    key2: {},
+    key3: {
+      type: 'number',
+      validator: (value) => value > 5,
+    },
+    key4: {},
+  };
+
+  const params = {
+    key1: [], // wrong type
+    key2: null, // valid
+    key3: 2, // fails validator
+    // key4 is missing
+  };
+  const validParams = {
+    key1: 'val1',
+    key2: null,
+    key3: 6,
+    key4: false,
+  };
+  assert.equal(validateRequired(required, params).length, 3, 'should return 3 errors');
+  assert.equal(
+    validateRequired(required, params).every((error) => error.constructor === Error),
+    true,
+    'should return Errors'
+  );
+  assert.equal(
+    validateRequired(required, validParams).length,
+    0,
+    'should return empty array when valid'
+  );
+  assert.throws(() => validateRequired(required, params, true), 'should throw when flag set');
+  assert.end();
+});
+
+test(`toSatoshi`, (assert) => {
+  const { toSatoshi } = Helpers();
+  assert.equal(toSatoshi(391), 0.00000391);
+  assert.equal(toSatoshi(100), Constants.ONE_HUNDRED_SHATOSHIS);
+  assert.equal(toSatoshi(1), Constants.ONE_SHATOSI);
+  assert.end();
+});
+
+test(`msToDatetime`, (assert) => {
+  const { msToDatetime } = Helpers();
+  assert.true(msToDatetime(Date.now()), 'can format for timezone');
+  assert.end();
+});
